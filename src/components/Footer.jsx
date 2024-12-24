018e8da90da91333edf4453e4f8fc13bbcf0a7eb
Awanthi Malawanage <121420903+Awanthiy@users.noreply.github.com>
2024-12-24 17:58:06 +0530

    Initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+         Awanthi Malawanage
+    </footer>
+  )
+}
+
+export default Footer
---
+++ src/components/WorkoutSessions.jsx
@@ -0,0 +1,45 @@
+ import React from 'react'
+ 
+ const WorkoutSessions = () => {
+   return (
+     <section className='workout_session'>
+      <div className="wrapper">
+      <h1>TOP WORKOUT SESSIONS</h1>
+      <p>
+      Class diagrams are a type of UML (Unified Modeling Language) diagram used in software engineering to visually represent the structure and relationships of classes within a system
+
+      </p>
+      <img src="/img5.jpg " alt="workout" />
+      </div>
+
+      <div className="wrapper">
+      <h1>FEATURED BOOTCAMPS</h1>
+      <p>
+      Class diagrams are a type of UML (Unified Modeling Language) diagram used in software engineering to visually represent the structure and relationships of classes within a system
+
+      </p>
+       <div className="bootcamps">
+        <div>
+          <h4>Class diagrams are a type of </h4>
+          <p>Class diagrams are a type of UML (Unified Modeling Language) diagram used in software engineering to visually represent the structure and relationships of classes within a </p>
+        </div>
+        <div>
+          <h4>Class diagrams are a type of </h4>
+          <p>Class diagrams are a type of UML (Unified Modeling Language) diagram used in software engineering to visually represent the structure and relationships of classes within a </p>
+        </div>
+        <div>
+          <h4>Class diagrams are a type of </h4>
+          <p>Class diagrams are a type of UML (Unified Modeling Language) diagram used in software engineering to visually represent the structure and relationships of classes within a </p>
+        </div>
+        <div>
+          <h4>Class diagrams are a type of </h4>
+          <p>Class diagrams are a type of UML (Unified Modeling Language) diagram used in software engineering to visually represent the structure and relationships of classes within a </p>
+        </div> 
+       </div>
+      </div>
+     </section>
+   )
+ }
+ 
+ export default WorkoutSessions
+ 
